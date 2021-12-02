5a7978963d77fc1ba4bd5a628e0dff83520e5b1c
Drake Axelrod <drake@draxel.io>
2021-12-02 20:25:11 +0100

    Add ci for deployment

--- index.js
+++ index.js
@@ -5,7 +5,36 @@ let path = require('path')
 let bodyParser = require('body-parser')
 require('dotenv').config()
 
-let databaseVariable = process.env.DATABASE_URL
+const { log } = console;
+const client = mqtt.connect({
+  hostname: process.env.BROKER_URI,
+  username: process.env.BROKER_USERNAME,
+  password: process.env.BROKER_PASSWORD,
+  protocol: "wss",
+});
+
+client.on("connect", (ack, err) => {
+  if (!err) {
+    console.log("connected");
+    // setInterval(
+    //   () => client.publish("booking", "message from booking-management"),
+    //   1000
+    // );
+  } else {
+    console.log(err);
+  }
+});
+
+// client.subscribe("gateway");
+
+client.on("message", (topic, message) => {
+  log(message.toString());
+  if (message.toString() === "stop") {
+    client.end();
+  }
+});
+
+let databaseVariable = process.env.MONGODB_URI
 let port = process.env.port || 3000
 
 mongoose.connect(databaseVariable, (error) => {
