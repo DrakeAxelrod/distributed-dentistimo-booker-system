0b94cf7340f606f8c3c73c518385e1099aec46d5
Drake Axelrod <drake@draxel.io>
2021-12-15 16:46:38 +0100

    Add error handling, QOS implementation
    Co-authored-by: robinhan <robinhan@student.chalmers.se>
    Co-authored-by: klarasve <klarasve@student.chalmers.se>
    Co-authored-by: asiya <asiya@student.chalmers.se>
    Co-authored-by: johanaxe <johanaxe@student.chalmers.se>
    Co-authored-by: simonar <simonar@student.chalmers.se>
    Co-authored-by: jidarv <jidarv@student.chalmers.se>
    
    Error handling on all systems, moved qos to 2 for all systems, implemented pipe and filter via the booking system and the gateway

--- src/controllers/bookings.js
+++ src/controllers/bookings.js
@@ -1,71 +1,6 @@
 const bookings = require("../models").bookings;
 const clinics = require("../models").clinics;
 
-const parseDate = (day) => {
-  //'10:00-16:00'
-  const times = day.split("-");
-  const start = times[0].split(":");
-  const end = times[1].split(":");
-  return {
-    start: {
-      hour: start[0],
-      minute: start[1],
-    },
-    end: {
-      hour: end[0],
-      minute: end[1],
-    },
-  };
-};
-
-const getAllBookingsForDay = (msg, booked) => {
-  bookedTimes = [];
-  booked.forEach((e) => bookedTimes.push(e.time.start));
-  const theDay = msg.clinic.openinghours[msg.date.dayName.toLowerCase()];
-  const available = [];
-  const day = parseDate(theDay);
-  const start = parseInt(day.start.hour);
-  const end = parseInt(day.end.hour);
-  for (let i = start; i <= end; i++) {
-    let hasHour = false;
-    let hasThirty = false;
-    let hasZeroZero = false;
-    bookedTimes.forEach((e) => {
-      if (e.hour === i) {
-        hasHour = true;
-        if (e.minute === 0) {
-          hasZeroZero = true;
-        }
-        if (e.minute === 30) {
-          hasThirty = true;
-        }
-      }
-    });
-    if (!hasZeroZero) {
-      available.push({
-        clinic: msg.clinic,
-        patient: {},
-        date: msg.date,
-        time: {
-          start: { hour: i, minute: 00 },
-          end: { hour: i, minute: 30 },
-        },
-      });
-    }
-    if (!hasThirty) {
-      available.push({
-        clinic: msg.clinic,
-        patient: {},
-        date: msg.date,
-        time: {
-          start: { hour: i, minute: 30 },
-          end: { hour: i + 1, minute: 00 },
-        },
-      });
-    }
-  }
-  return available;
-};
 
 const findClinic = async (name) => {
   return await clinics.findOne({ name: name })
@@ -74,32 +9,27 @@ const findClinic = async (name) => {
 const confirmAppointment = async (m) => {
   const obj = JSON.parse(m)
   const clinic = await findClinic(obj.clinic);
-  const booking = await bookings.create({
-    clinic: clinic._id,
-    patient: obj.patient,
-    time: obj.time,
-    date: obj.date,
-  });
-  booking.clinic = clinic
-  return JSON.stringify(booking);
+  const booking = await bookings
+    .create({
+      clinic: clinic._id,
+      patient: obj.patient,
+      time: obj.time,
+      date: obj.date,
+    })
+    .then((res) => res)
+    .catch((err) => ({ confirmed: false }));
+  const allBookings = { ...booking._doc };
+  if (!allBookings.hasOwnProperty["confirmed"]) {
+    allBookings["confirmed"] = true
+    allBookings.clinic = clinic;
+  }
+  return JSON.stringify(allBookings);
 };
 
-const allAvailableForDayAppointments = async (m) => {
+const allBookedAppointments = async (m) => {
   const msg = JSON.parse(m);
-  const clinicName = msg.clinic.name;
-  const res = await bookings.find().populate("clinic");
-
-  const bookedAppointments = res.filter((booked) => {
-    const sameName = booked.clinic.name === clinicName;
-    const sameYear = msg.date.year === booked.date.year;
-    const sameDay = msg.date.day === booked.date.day;
-    const sameMonth = msg.date.month === booked.date.month;
-    const sameDate = sameDay && sameMonth && sameYear;
-    if (sameName && sameDate) return booked;
-  });
-  const available = getAllBookingsForDay(msg, bookedAppointments);
-  //available.forEach(e => console.log(e.time.start))
-  return JSON.stringify(available);
+  const res = await bookings.find().populate("clinic").then(res => res).catch(err => []);
+  return JSON.stringify({ message: msg, booked: res });
 };
 
 const isClinics = async () => {
@@ -109,7 +39,6 @@ const isClinics = async () => {
 
 module.exports = {
   confirmAppointment: confirmAppointment,
-  allAvailableForDayAppointments: allAvailableForDayAppointments,
+  allBookedAppointments: allBookedAppointments,
   isClinics: isClinics
-  // reserveAppointment: reserveAppointment
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -14,21 +14,26 @@ const responsePath = "api/gateway/bookings"
 
 client.subscribe(basePath)
 const topics = [
-    { topic: "confirm", qos: 0 },
-    { topic: "all", qos: 0 },
-    { topic: "available", qos: 0 }
+    { topic: "confirm", qos: 2 },
+    { topic: "all", qos: 2 },
+    { topic: "available", qos: 2 }
 ];
 topics.forEach(route => {
     client.subscribe(basePath + "/" + route.topic, { qos: route.qos })
 });
 
-client.on("message", (t, m) => {
+const breaker = new CircuitBreaker(client.on("message", (t, m) => {
     const msg = m.toString()
     const topic = t.replace(basePath + "/", "");
     const breaker = new CircuitBreaker(client.emit(topic, msg), options);
     const result = breaker.fire().then(res => res).catch(console.error)
     //client.emit(topic, msg)
+}), {
+  timeout: 3000, // If our function takes longer than 3 seconds, trigger a failure
+  errorThresholdPercentage: 25, // When 25% of requests fail, trip the circuit
+  resetTimeout: 10000, // After 10 seconds, try again.
 });
+breaker.fire();
 
 client.on('confirm', async(m) => {
     const booking = await controllers.bookings.confirmAppointment(m)
@@ -40,12 +45,11 @@ client.on('confirm', async(m) => {
         .fire()
         .then((res) => res)
         .catch(console.error);
-    //client.publish(responsePath + "/confirm", booking)
 })
 
 client.on("available", async (m) => {
   const res = await controllers.bookings
-    .allAvailableForDayAppointments(m)
+    .allBookedAppointments(m)
     .then((res) => res)
     .catch((error) => {
       if (error) {
@@ -60,7 +64,6 @@ client.on("available", async (m) => {
         .fire()
         .then((res) => res)
         .catch(console.error);
-  //client.publish(responsePath + "/available", JSON.stringify(res));
 });
 
 module.export = client;
